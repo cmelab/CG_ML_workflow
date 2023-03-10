7c21bf561810b87b345775b89fd590b5c5125a14
marjanalbouye <marjan.albouye73@gmail.com>
2023-03-10 13:09:59 -0700

    update slurm scripts.

--- src/sweep.sh
+++ src/sweep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=stack-lr01
+#SBATCH --job-name=sweep
 #SBATCH -N 1
 #SBATCH -n 1
 
@@ -11,7 +11,7 @@ conda activate polybinder
 
 python run.py -data_path "/home/marjanalbooyeh/logs/pps_rigid/2023-03-09-18:47:47/dataset" \
  -project "NN_multi_PPS" -group "10_pps" \
- -lr 0.1 -batch 128 -hidden_dim 64 -epochs 10000 -n_layer 3 -act_fn "Tanh" -mode "single" \
+ -lr 0.1 -batch 128 -hidden_dim 64 -epochs 10000 -n_layer 3 -act_fn "Tanh" -mode "sweep" \
  -notes "appended input, dataset (neighbors) 1 row in data for all particles, num_compounds=10" \
  -inp_mode "append"
 
